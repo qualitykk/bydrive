665b93ef56fb3e09c9322bddc8e91f62a24e6cf3
Kilian <noqualitycont@gmail.com>
2024-01-07 20:53:07 +0100

    First basic pickup, boost recharge

---
+++ code/Race/Pickups/BoostPickup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bydrive;
+
+[Category("Race")]
+public class BoostPickup : RacerPickup
+{
+	public override bool OnPickup( VehicleController vehicle )
+	{
+		float maxBoost = vehicle.GetBoostDuration();
+
+		if(vehicle.RemainingBoost < maxBoost )
+		{
+			vehicle.RemainingBoost = maxBoost;
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ code/Race/Pickups/RacerPickup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bydrive;
+
+public abstract class RacerPickup : Component, Component.ITriggerListener
+{
+	public const float DEFAULT_RESPAWN_TIME = 10f;
+	[Property] public float RespawnTime { get; set; } = DEFAULT_RESPAWN_TIME;
+	public bool Available { get; set; } = true;
+	public TimeSince TimeSincePickup { get; set; }
+	public abstract bool OnPickup( VehicleController vehicle );
+	protected override void OnUpdate()
+	{
+		if(!Available)
+		{
+			if(TimeSincePickup > RespawnTime)
+			{
+				Available = true;
+				OnBecomeAvailable();
+			}
+		}
+	}
+	protected virtual void OnBecomeAvailable()
+	{
+	}
+	protected virtual void OnBecomeUnavailable()
+	{
+	}
+
+	void ITriggerListener.OnTriggerEnter( Collider other )
+	{
+		if ( !Available )
+			return;
+
+		var vehicle = other.Components.GetInAncestorsOrSelf<VehicleController>();
+		if ( !OnPickup( vehicle ) )
+			return;
+
+		Available = false;
+		TimeSincePickup = 0;
+	}
+
+	void ITriggerListener.OnTriggerExit( Collider other )
+	{
+	}
+}
