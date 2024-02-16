924a0f80154d11e2e0a4cd42203bd802d8b1848a
Kilian <noqualitycont@gmail.com>
2024-02-16 23:34:33 +0100

    Start story mode overworld implementation

---
+++ code/StoryMode/OverworldController.cs
@@ -0,0 +1,166 @@
+﻿using Sandbox.Citizen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bydrive;
+
+public class OverworldController : Component
+{
+	[Property] public CharacterController WalkMovement { get; set; }
+	[Property] public CitizenAnimationHelper Animator { get; set; }
+	[Property] public float Speed { get; set; } = 220f;
+	[Property] public float RotationSpeed { get; set; } = 5f;
+	[Property] public Vector3 CameraOffset { get; set; }
+	Rotation cameraRotation;
+
+	Vector3 wishVelocity;
+	Rotation wishRotation;
+
+	RealTimeSince lastGrounded;
+	RealTimeSince lastUngrounded;
+	protected override void OnStart()
+	{
+		cameraRotation = Transform.Rotation;
+	}
+	float GetFriction()
+	{
+		if ( WalkMovement.IsOnGround ) return 6.0f;
+
+		// air friction
+		return 0.2f;
+	}
+	protected override void OnUpdate()
+	{
+		Look();
+		Move();
+		UpdateAnimation();
+	}
+	private void Look()
+	{
+		const float CAMERA_MAX_PITCH = 15f;
+
+		Angles cameraAngle = cameraRotation;
+		cameraAngle += Input.AnalogLook;
+		cameraAngle.pitch = cameraAngle.pitch.Clamp( -CAMERA_MAX_PITCH, CAMERA_MAX_PITCH );
+		cameraAngle.roll = 0.0f;
+
+		cameraRotation = cameraAngle;
+	}
+
+	private void Move()
+	{
+		if ( WalkMovement is null )
+			return;
+
+		CharacterController cc = WalkMovement;
+
+		Vector3 halfGravity = Scene.PhysicsWorld.Gravity * Time.Delta * 0.5f;
+
+		wishVelocity = Input.AnalogMove;
+
+
+		if ( !wishVelocity.IsNearlyZero() )
+		{
+			wishVelocity = new Angles( 0, cameraRotation.Yaw(), 0 ).ToRotation() * wishVelocity;
+			wishVelocity = wishVelocity.WithZ( 0 );
+			wishVelocity = wishVelocity.ClampLength( 1 );
+			wishVelocity *= Speed;
+
+			if ( !cc.IsOnGround )
+			{
+				wishVelocity = wishVelocity.ClampLength( 50 );
+			}
+		}
+
+
+		cc.ApplyFriction( GetFriction() );
+
+		if ( cc.IsOnGround )
+		{
+			cc.Accelerate( wishVelocity );
+			cc.Velocity = WalkMovement.Velocity.WithZ( 0 );
+		}
+		else
+		{
+			cc.Velocity += halfGravity;
+			cc.Accelerate( wishVelocity );
+
+		}
+
+		cc.Move();
+
+		if ( !cc.IsOnGround )
+		{
+			cc.Velocity += halfGravity;
+		}
+		else
+		{
+			cc.Velocity = cc.Velocity.WithZ( 0 );
+		}
+
+		if ( cc.IsOnGround )
+		{
+			lastGrounded = 0;
+		}
+		else
+		{
+			lastUngrounded = 0;
+		}
+
+		if(wishVelocity != default)
+		{
+			wishRotation = Rotation.LookAt( wishVelocity );
+		}
+		Transform.Rotation = Rotation.Slerp(Transform.Rotation, wishRotation, Time.Delta * RotationSpeed);
+	}
+
+	private void UpdateCamera()
+	{
+		CameraComponent camera = GameManager.ActiveScene.Camera;
+		if ( camera == null ) return;
+
+		var targetCameraPos = Transform.Position + CameraOffset * Rotation.LookAt(cameraRotation.Forward);
+
+		// smooth view z, so when going up and down stairs or ducking, it's smooth af
+		if ( lastUngrounded > 0.2f )
+		{
+			targetCameraPos.z = camera.Transform.Position.z.LerpTo( targetCameraPos.z, RealTime.Delta * 25.0f );
+		}
+
+		camera.Transform.Position = targetCameraPos;
+		camera.Transform.Rotation = cameraRotation;
+		camera.FieldOfView = 90f;
+	}
+
+	protected override void OnPreRender()
+	{
+		UpdateCamera();
+	}
+
+	private void UpdateAnimation()
+	{
+		if ( Animator is null ) return;
+
+		var wv = wishVelocity.Length;
+
+		Animator.WithWishVelocity( wishVelocity );
+		Animator.WithVelocity( WalkMovement.Velocity );
+		Animator.IsGrounded = WalkMovement.IsOnGround;
+
+		/*
+		var lookDir = eyeRotation.Forward * 1024;
+		Animator.WithLook( lookDir, 1, 0.5f, 0.25f );
+		*/
+	}
+
+	protected override void DrawGizmos()
+	{
+		if ( !Gizmo.IsSelected ) return;
+
+		Gizmo.Draw.Color = Color.Blue;
+		Gizmo.Draw.SolidSphere( CameraOffset, 4f );
+	}
+}
---
+++ code/StoryMode/OverworldManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bydrive;
+
+public class OverworldManager : Component
+{
+	[Property] public PrefabFile PlayerPrefab { get; set; }
+	protected override void OnAwake()
+	{
+		if(PlayerPrefab == null)
+		{
+			Log.Error( "Overworld without player???" );
+			return;
+		}
+
+		GameObject player = new();
+		player.ApplyPrefab( PlayerPrefab );
+
+		// TODO: Get story based spawn pos
+		GameObject spawn = Scene.GetAllComponents<SpawnPoint>().FirstOrDefault().GameObject;
+		player.Transform.World = spawn.Transform.World;
+	}
+}
