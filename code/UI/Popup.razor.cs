fd08ed2433480fad9de1a84840efa60de2c43023
Kilian <noqualitycont@gmail.com>
2024-02-29 22:50:31 +0100

    Reorganize UI folder structure

---
+++ code/UI/Popup.razor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bydrive;
+
+public partial class Popup : PanelComponent
+{
+	public struct Line
+	{
+		public string Message { get; set; }
+		public Color Color { get; set; }
+		public string Icon { get; set; }
+
+		public Line( string message, Color color, string icon = "" )
+		{
+			Message = message;
+			Color = color;
+			Icon = icon;
+		}
+	}
+
+	public static Popup Current { get; private set; }
+	private List<Line> popupQueue { get; set; } = new();
+
+	public static void Add( Line notification )
+	{
+		Current?.popupQueue.Add( notification );
+	}
+
+	protected override void OnEnabled()
+	{
+		base.OnEnabled();
+		Current = this;
+	}
+	protected override void OnUpdate()
+	{
+		
+	}
+
+	protected override int BuildHash()
+	{
+		return HashCode.Combine( Time.Now );
+	}
+}
