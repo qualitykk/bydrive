101a770793e8f865b798e838dad05205468814d7
Kilian <noqualitycont@gmail.com>
2024-03-15 00:34:49 +0100

    Branding Update
    - Change button font to something more fitting
    - Update title image, show on pause screen too
    - Added missing developer credits

--- code/MainMenu/CreditsPage.razor.cs
+++ code/MainMenu/CreditsPage.razor.cs
@@ -5,10 +5,12 @@ public partial class CreditsPage
 {
 	private static readonly List<CreditsEntry> developmentEntries = new()
 	{
-		new(76561198835895479, "qualityk", "kilianq", "Main Developer")
+		new(76561198835895479, "qualityk", "kilianq", "Main Developer"),
+		new(76561198282489899, "Kanalratte", "kanalratte", "Models, Textures"),
+		new(76561198162417562, "SlimesMcDingle", "slimesmcdingle", "Models, Textures"),
 	};
 
-	private static List<CreditsEntry> attributionEntries = new()
+	private static readonly List<CreditsEntry> attributionEntries = new()
 	{ 
 		new(0, "SkyernAklea", "", "Car Engine Sounds"),
 		new(0, "inchadney", "", "City Soundscape")
